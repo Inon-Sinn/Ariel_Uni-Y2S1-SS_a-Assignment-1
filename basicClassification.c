c9846f2506df4ddda7d309ffce4393a59cb63949
Inon Sinn <inon.sinn@gmail.com>
2021-11-08 00:06:41 +0200

    isPrime Finished

--- basicClassification.c
+++ basicClassification.c
@@ -1,7 +1,13 @@
 #include "NumClass.h"
 
 int isPrime(int n){
-    return 0;
+    int i,m=n/2;       
+    for(i=2;i<=m;i++)    
+    {    
+        if(n%i==0)      
+            return 0;      
+    }       
+    return 1;  
 }
 
 int isStrong(int n){
--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include "NumClass.h"
 
+int isPrime(int n);
+
+int isArmstrong(int n){
+    return 0;
+}
+
+int isPalindrome(int n){
+    return 0;
+}
+
+int isStrong(int n);
+
+int fact(int r);
+
 
 
 int main(){ 
@@ -12,7 +26,7 @@ int main(){
     //Armstrong numbers
     printf("The Armstrong numbers are:");
     for(start;start<=end;start++){
-        if(isArmstrong(start))
+        if(isStrong(start))
             printf(" %d",start);
     }
     printf("\n");
@@ -26,20 +40,63 @@ int main(){
     //Prime Numbers
     printf("The Prime numbers are:");
     for(start;start<=end;start++){
-        if(isPalindrome(start))
+        if(isStrong(start))
             printf(" %d",start);
     }
     printf("\n");
     //Strong Numbers
     printf("The Strong numbers are:");
     for(start;start<=end;start++){
-        if(isPalindrome(start))
+        if(isStrong(start))
             printf(" %d",start);
     }
     printf("\n");
     return 0;
 
 }
+
+
+int isStrong(int n){
+    int x; 
+    int sum=0;  
+    printf("Enter a number");  
+    scanf("%d",&x);  
+    int k=x;  
+    int r;  
+    while(k!=0)  
+    {  
+        r=k%10;  
+        int f=fact(r);  
+        k=k/10;  
+        sum=sum+f;  
+    }  
+    if(sum==x)  
+    {  
+        printf("\nNumber is a strong");  
+    }  
+    else  
+    {  
+        printf("\nNumber is not a strong");  
+    }  
+    return 0; 
+}
+
+
+int fact(int r){  
+    int mul=1;  
+    for(int i=1;i<=r;i++)  
+    {  
+        mul=mul*i;  
+    }  
+    return mul;  
+      
+}
+
+
+
+
+
+
 
 
 
